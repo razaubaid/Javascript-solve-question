da49160092045b7126589e7c9af1601030739577
Ubaid Raza Qureshi <160884055+razaubaid@users.noreply.github.com>
2024-03-08 19:49:52 -0800

    practice

---
+++ app.js
@@ -0,0 +1,49 @@
+//ALERTS
+alert("Error! Please enter a valid password.");
+alert("Welcom to Js Land...\nHappy Coding!");
+alert("Welcom to Js Land...");
+alert("Happy Coding!");
+console.log("Hello...I can run through my web browser's console");
+//VARIABLES FOR STRINGS
+var userName = "Ubaid Raza ";
+var age = " 20 Years old";
+alert(userName);
+var age = "20 Years old";
+alert(age);
+var skill = " Web an Moblie Application Devloper";
+alert(skill);
+var scr = "pizza\npizz\npiz\npi\np";
+alert(scr);
+var message = "I am trying to learn from the Book A smart way to learn javaScript";
+alert(message);
+var store = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert (store);
+//VARIABLES FOR NUMBER
+var age = "20";
+alert("I am " + age + " years old");
+var counterContainer = document.querySelector(".website-counter");
+var resetButton = document.querySelector("#reset");
+var visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+alert("You have visited this site: " + visitCount);
+var person = 
+age = 20 ;
+year = 2003 ;
+console.log (person + year);
+var VisitorsName = "Ubaid Raza";
+var Quantity = "5";
+var ProductTitle = "T-shirt";
+console.log (VisitorsName + " ordered " + 5 + ProductTitle );
+
+
+
+
+
